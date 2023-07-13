ab3b35b25819b746d0ad13989487f3053a5cab56
AndriyCh1 <chekanovskiyacc@gmail.com>
2023-07-13 11:03:26 +0300

    selection sort

---
+++ selection-sort.js
@@ -0,0 +1,33 @@
+function swapElements(firstIndex, secondIndex, arr) {
+  const firstElement = arr[firstIndex];
+
+  arr[firstIndex] = arr[secondIndex];
+  arr[secondIndex] = firstElement;
+
+  return arr;
+}
+
+function selectionSort(arr) {
+  let currentSmallestIndex;
+
+  for (let i = 0; i < arr.length; i++) {
+    currentSmallestIndex = i;
+
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[currentSmallestIndex] > arr[j]) {
+        currentSmallestIndex = j;
+      }
+    }
+
+    if (currentSmallestIndex !== i) {
+      swapElements(i, currentSmallestIndex, arr);
+    }
+  }
+
+  return arr;
+}
+
+const input = [5, 2, 4, 6, 1, 3];
+const result = selectionSort(input);
+console.log(input);
+console.log(result);
